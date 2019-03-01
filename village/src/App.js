6352ac5b6c760b351d3fe8e2b5926d3b2f83d5b3
Max Trestman <mtrestman1@android-4105f9b98c1d7f96.workspace.codeandsupply.co>
2019-03-01 11:43:18 -0500

    Routed smurfs and smurf form to respective pages

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -26,8 +27,9 @@ componentDidMount() {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+       
+       <Route exact path='/' render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+       <Route path='/smurf-form' component={SmurfForm} />
       </div>
     );
   }
